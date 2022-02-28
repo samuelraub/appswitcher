6afd469f6d630f0f406f8fd45e44aae5ab7bc63c
Samuel Raub <samuel.raub@gmail.com>
2022-02-28 13:49:39 +0100

    Initial Commit

---
+++ AppSwitcher/AppSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwitcherApp.swift
+//  AppSwitcher
+//
+//  Created by Samuel Raub on 28.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AppSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwitcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSwitcher
+//
+//  Created by Samuel Raub on 28.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
